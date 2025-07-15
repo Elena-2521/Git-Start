4d8074379fbf8ccf0939d228eb278c1e7b83aa43
Alina Bukhari <elenashah975@gmail.com>
2025-07-15 17:18:42 +0500

    Version 1 restored

--- change.js
+++ change.js
@@ -1,3 +1,2 @@
 // Editing our existing commit
 console.log("change.js")
-console.log("We made a change for version 3")
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 // Building a version history
 console.log("index.js")
-console.log("Version-3")
+console.log("Version-1")
